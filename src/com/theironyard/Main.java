463e63abeb25a8d885d356df8fc6af050bf390a0
benjamindrake <spcbdrake@yahoo.com>
2015-10-28 11:36:30 -0400

    add test data and show threads

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -4,6 +4,7 @@ import spark.ModelAndView;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -13,13 +14,37 @@ public class Main {
         HashMap<String, User> users = new HashMap();
         ArrayList<Message> messages = new ArrayList();
 
+        addTestUsers(users);
+        addTestMessages(messages);
+
         Spark.get(
                 "/",
                 ((request, response) -> {
+                    ArrayList<Message> threads = new ArrayList();
+                    for (Message message : messages) {
+                        if (message.replyId == -1) {
+                            threads.add(message);
+                        }
+                    }
+
                     HashMap m = new HashMap();
+                    m.put("threads", threads);
                     return new ModelAndView(m, "threads.html");
                 }),
                 new MustacheTemplateEngine()
         );
+    }
+
+    static void addTestUsers(HashMap<String, User> users) {
+        users.put("Alice", new User());
+        users.put("Bob", new User());
+        users.put("Charlie", new User());
+    }
+    static void addTestMessages (ArrayList<Message> messages) {
+        messages.add(new Message(0,-1, "Alice", "This is a thread!"));
+        messages.add(new Message(1,-1, "Bob", "This is a thread!"));
+        messages.add(new Message(2, 0, "Charlie", "Cool thread, Alice!"));
+        messages.add(new Message(3, 2, "Alice", "Fuck off Charlie"));
+
     }
 }
